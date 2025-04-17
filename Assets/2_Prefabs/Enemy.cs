79b372726ef87b2baeae6ec1fbb5e0fb91da8cfa
connor-sae <connor.westhuizen@student.saedubai.com>
2025-04-17 09:32:14 +0400

    Added Enemy Script

---
+++ Assets/2_Prefabs/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IAlertable
+{
+    public void Alert(Vector3 alertOrigin)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
