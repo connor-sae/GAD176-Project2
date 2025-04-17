3a0726c70fe70df6f8a6258d065ec50382e9b7e5
connor-sae <connor.westhuizen@student.saedubai.com>
2025-04-17 15:33:21 +0400

    Completed Enemy Script
     - patrols between given points
     - can detect players

--- Assets/1_Scripts/Connor/VisionConeDetection.cs
+++ Assets/1_Scripts/Connor/VisionConeDetection.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class VisionConeDetection : MonoBehaviour
 {
-    protected bool Detect(Vector3 dectectionOrigin, Collider targetCollider, float viewConeAngle, float viewConeDistance)
+    [SerializeField] float viewConeAngle = 45;
+    [SerializeField] float viewConeDistance = 4;
+    [SerializeField] Transform viewOrigin;
+
+    /// <summary>
+    /// Detects whether the given Collider is within the view Cone and is unobstructed from the view Origin
+    /// </summary>
+    /// <returns>whether the object is detected</returns>
+    protected bool Detect(Collider targetCollider)
     {
-        Vector3 targetDirection = targetCollider.transform.position - dectectionOrigin;
+        Vector3 targetDirection = targetCollider.transform.position - viewOrigin.position;
         //
         float angleDifference = Vector3.Angle(transform.forward, targetDirection);
 
         if (angleDifference < viewConeAngle && targetDirection.magnitude < viewConeDistance)
         {
-            Physics.Raycast(dectectionOrigin, targetDirection, out RaycastHit hit, targetDirection.magnitude);
+            Physics.Raycast(viewOrigin.position, targetDirection, out RaycastHit hit, targetDirection.magnitude);
             return hit.collider == targetCollider;
         }
         return false;
