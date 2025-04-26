a5bb0389ff3cb71f7e263c05603684b7d6031997
Daniel Achkar <Daniel.achkar@yahoo.com>
2025-04-26 21:34:47 +0400

    Added Enemy rotate and Move towards player
    The enemy now rotates and moves towards the player when the sus threshold is met.

--- Assets/1_Scripts/Daniel/EnemyDetection.cs
+++ Assets/1_Scripts/Daniel/EnemyDetection.cs
@@ -15,11 +15,15 @@ namespace DanielGAD176
         [SerializeField] float susMeter = 0f;
         [SerializeField] float susIncreaseRate = 10f;
         [SerializeField] float maxSus = 100f;
+
+        [SerializeField] float moveSpeed;
+        [SerializeField] float rotateSpeed;
         
         // to use this code make your enemies inherit form this and use FixedUpdate for VisualDetection (IMPORTANT).
         void Start()
         {
-            
+            moveSpeed = 1f;
+            rotateSpeed = 2f;
              //You need to change the tag of the player to "Player" for this to work.
             if (player == null)
             {
@@ -43,10 +47,11 @@ namespace DanielGAD176
             }
             else
             {
-                susMeter -= susIncreaseRate * 0.5f * Time.deltaTime;
+                susMeter -= susIncreaseRate * 0.7f * Time.deltaTime;
                 susMeter = Mathf.Max(susMeter, 0f);
                 Debug.Log(susMeter);
-            } 
+            }
+            SusBehaviour();
         }
 
         // fuction that checks if player is within view cone of enemy using distance and angles.
@@ -72,6 +77,51 @@ namespace DanielGAD176
             }
             return false;
         }
+
+        // fuction that handles suspecious behaviour.
+        protected virtual void SusBehaviour()
+        {
+            if (susMeter > 50f && susMeter < 75f)
+            {
+                RotateTowardsPlayer();
+            }
+            else if ( susMeter >= 75f && susMeter < maxSus)
+            {
+                RotateTowardsPlayer();
+                MoveTowardsPlayer();
+            }
+            else if (susMeter >= maxSus)
+            {
+                CatchPlayer();
+            }
+        }
+
+        protected virtual void RotateTowardsPlayer()
+        {
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+
+            if (direction.magnitude > 0.1)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotateSpeed);
+            }
+                
+        }
+
+        protected virtual void MoveTowardsPlayer()
+        {
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+            direction.y = 0;
+
+            transform.position += direction * moveSpeed * Time.deltaTime;
+        }
+
+        // Could be changed based on game behaviour.
+        protected virtual void CatchPlayer()
+        {
+            Debug.Log("Player Caught!!!");
+        }
+
         // Draws detection lines for reference.     
         private void OnDrawGizmos()
         {
